10480044d9ba8d0d466efa8fbfb35cc3e2625416
Tuppurainen_PSPR <488-Tuppurainen_PSPR@users.noreply.gitlab.vky>
2023-06-29 11:58:22 +0300

    improvements

--- converter.py
+++ converter.py
@@ -3,16 +3,18 @@ from pathlib import Path
 import os
 
 path = Path(os.path.dirname(os.path.realpath(__file__))+"\ImageMagick")
-if not path.exists():
+while not path.exists():
     path = Path(input("Enter ImageMagick-folder location: "))
 
 inp = input("Enter input folder (empty for currPath\in): ")
 if inp == "":
-    inp=Path(os.path.dirname(path)+"\in")
+    inp=os.path.dirname(path)+"\in"
+inp = Path(inp)
 
 out = input("Enter output folder (empty for currPath\out): ")
 if out == "":
-    out=Path(os.path.dirname(path)+"\out")
+    out=os.path.dirname(path)+"\out"
+out = Path(out)
 
 ext = input("Enter file extension (empty for png): ")
 if ext == "":
@@ -32,5 +34,5 @@ counter = 0
 for file in inp.glob("*"):
     if orig:
         name = file.stem
-    subprocess.run(f"{path}\convert.exe in\{file.name} {resize} {out}\{name + ('.'+str(counter) if (counter > 0) and (not orig) else '')}.{ext}", shell=True)
+    subprocess.run(f"{path}\convert.exe {inp}\{file.name} {resize} {out}\{name + ('.'+str(counter) if (counter > 0) and (not orig) else '')}.{ext}", shell=True)
     counter+=1
