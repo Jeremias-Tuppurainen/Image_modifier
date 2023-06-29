514652fbc7134d80a952d4f81021c7e1d2372982
Tuppurainen_PSPR <488-Tuppurainen_PSPR@users.noreply.gitlab.vky>
2023-06-29 12:31:14 +0300

    reformatting

--- converter.py
+++ converter.py
@@ -1,20 +1,14 @@
-import subprocess
 from pathlib import Path
+import subprocess
 import os
 
-path = Path(os.path.dirname(os.path.realpath(__file__))+"\ImageMagick")
-while not path.exists():
-    path = Path(input("Enter ImageMagick-folder location: "))
+path = Path(os.path.dirname(os.path.realpath(__file__)))
+magic_path = Path(path, "ImageMagick")
+while not magic_path.exists():
+    magic_path = Path(input("Enter ImageMagick-folder location: "))
 
-inp = input("Enter input folder (empty for currPath\in): ")
-if inp == "":
-    inp=os.path.dirname(path)+"\in"
-inp = Path(inp)
-
-out = input("Enter output folder (empty for currPath\out): ")
-if out == "":
-    out=os.path.dirname(path)+"\out"
-out = Path(out)
+inp = Path(x) if not (x := input("Enter input folder (empty for currPath\in): ")) == "" else Path(path, "in")
+out = Path(y) if not (y := input("Enter output folder (empty for currPath\out): ")) == "" else Path(path, "out")
 
 ext = input("Enter file extension (empty for png): ")
 if ext == "":
@@ -34,5 +28,5 @@ counter = 0
 for file in inp.glob("*"):
     if orig:
         name = file.stem
-    subprocess.run(f"{path}\convert.exe {inp}\{file.name} {resize} {out}\{name + ('.'+str(counter) if (counter > 0) and (not orig) else '')}.{ext}", shell=True)
+    subprocess.run(f"{magic_path}\convert.exe {inp}\{file.name} {resize} {out}\{name + ('.'+str(counter) if (counter > 0) and (not orig) else '')}.{ext}", shell=True)
     counter+=1
