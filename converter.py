c041dc2a15ae70eac678b3f775ae5c3deb7f39b9
Tuppurainen_PSPR <488-Tuppurainen_PSPR@users.noreply.gitlab.vky>
2023-07-03 10:56:03 +0300

    cleaned and improved code

--- converter.py
+++ converter.py
@@ -9,12 +9,14 @@ while not magic_path.exists():                                                  
 
 inp = Path(x) if not (x := input("Enter input folder (empty for currPath\in): ")) == "" else Path(path, "in")           # if user input is empty put default path for input folder
 out = Path(x) if not (x := input("Enter output folder (empty for currPath\out): ")) == "" else Path(path, "out")        # same but for output folder
-ext if not (ext := input("Enter file extension (empty for no): ")) == "" else (ext := True)                             # change file extension, if user input is empty keep original extension
-name if not (name := input("Rename? (empty for no): ")) == "" else (name := True)                                       # if input was empty keeps the original name, else stores the new given name
+ext = input("Enter file extension (empty for no): ")                                                                    # change file extension, if user input is empty keep original extension
+name = input("Rename? (empty for no): ")                                                                                # if input was empty keeps the original name, else stores the new given name
 resize if (resize := input("Resize? (empty for no, leave x in between): ")) == "" else (resize := "-resize " + resize)  # if input was empty keeps the original size, else stores the new given size
 
 out.mkdir(parents=True, exist_ok=True)                                                                                  # checks if the path is correct, if not it makes the correct new folders
 counter = 0                                                                                                             # counter for image names, when renaming
 for file in inp.glob("*"):                                                                                              # goes through all the files in the input folder
-    subprocess.run(f"{magic_path}\convert.exe {inp}\{file.name} {resize} {out}\{(name if not (name==True) else file.stem) + ('.'+str(counter) if (counter > 0) and not(name == True) else '')}{'.'+ext if not (ext==True) else Path(file).suffix}", shell=True)  
+    outname = (name if name else file.stem) + ('.'+str(counter) if (counter > 0) and name else '')
+    outext = '.'+ext if ext else Path(file).suffix
+    subprocess.run(f"{magic_path}\convert.exe {inp}\{file.name} {resize} {out}\{outname}{outext}", shell=True)  
     counter+=1                                                                                                          # row above runs the convert.exe inside ImageMagick folder and gives it the necessary information.
